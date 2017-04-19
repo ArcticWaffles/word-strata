4036eb3156e1d3ce134ee9916a881779e28880ca
Tisa Foster <tisa.foster@gmail.com>
2017-04-19 09:34:29 -0600

    Used test cases to simplify SolverTests.

--- NunitTests/SolverTests.cs
+++ NunitTests/SolverTests.cs
@@ -13,97 +13,71 @@ namespace NUnitTests
     [TestFixture]
     class SolverTests
     {
-        Board board2x2;
-        static Board board3x3;
-
-        static HashSet<string> dictionaryA;
-        static HashSet<string> dictionaryB;
-        HashSet<string> dictionaryC;
-
-        private static HashSet<string>[] ValidDictionaries = new HashSet<string>[] {
-            dictionaryA,
-            dictionaryB,
-        };
-
-        [SetUp]
-        public void Init()
-        {
-            dictionaryA = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            {
+        static HashSet<String> dictionaryA = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        { 
             "bark",
             "bare",
             "bar",
             "cab"
-            };
+        };
 
-            dictionaryB = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            {
-            "back",
+        static HashSet<String> dictionaryB = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "cbd",
             "ark",
-            "crab",
-            "are",
-            "crabcake"
-            };
-            
-            dictionaryC = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            {                
+            "crab"
+        };
+
+        static HashSet<String> dictionaryC = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {                
             "carrot"
-            };
+        };
 
 
-            board2x2 = new Board(new char[,]
-            {
-                { 'a', 'b'},
-                { 'c', 'd'}
-            });
-
-            board3x3 = new Board(new char[,]
-            {
-                { 'a', 'b', 'd' },
-                { 'r', 'k', 'x' },
-                { 'e', 'x', 'x' }
-            });
-        }
-
-
-
-        // AnyWordExistsOnBoard DictionaryChecker tests
-        // TODO: Use test case attribute to test multiple scenarios in one test (Condense the following tests into one.)
-
-        [Test]
-        public void FindWordDC_WordExists2x2DA_ResultIsTrue()
+        static Board board2x2 = new Board(new char[,]
         {
-            var result = Solver.AnyWordExistsOnBoard(dictionaryA, board2x2);
+            { 'a', 'b'},
+            { 'c', 'd'}
+        });
+
+        static Board board3x3 = new Board(new char[,]
+        {
+            { 'a', 'b', 'd' },
+            { 'r', 'k', 'x' },
+            { 'e', 'x', 'x' }
+        });
+
+
+        // AnyWordExistsOnBoard tests (uses DictionaryChecker)
+
+        [Test, TestCaseSource("WordExistsCases")]
+        public void AnyWordExistsOnBoard_WordExists_ResultIsTrue(HashSet<string> dictionary, Board gameboard)
+        {
+            var result = Solver.AnyWordExistsOnBoard(dictionary, gameboard);
             Assert.That(result, Is.True);
         }
 
-        [Test]
-        public void FindWordDC_WordExists3x3DA_ResultIsTrue()
+        static object[] WordExistsCases =
         {
-            var result = Solver.AnyWordExistsOnBoard(dictionaryA, board3x3);
-            Assert.That(result, Is.True);
-        }
+            new object[] { dictionaryA, board2x2 },
+            new object[] { dictionaryA, board3x3 },
+            new object[] { dictionaryB, board3x3 },
+        };
 
-        [Test]
-        public void FindWordDC_WordExists3x3DB_ResultIsTrue()
+        [Test, TestCaseSource("WordDoesNotExistCases")]
+        public void AnyWordExistsOnBoard_WordDoesNotExist_ResultIsFalse(HashSet<string> dictionary, Board gameboard)
         {
-            var result = Solver.AnyWordExistsOnBoard(dictionaryB, board3x3);
-            Assert.That(result, Is.True);
-        }
-
-        [Test]
-        public void FindWordDC_WordDoesNotExist2x2DC_ResultIsFalse()
-        {
-            var result = Solver.AnyWordExistsOnBoard(dictionaryC, board2x2);
+            var result = Solver.AnyWordExistsOnBoard(dictionary, gameboard);
             Assert.That(result, Is.False);
         }
 
-        [Test]
-        public void FindWordDC_WordDoesNotExist3x3DC_ResultIsFalse()
+        static object[] WordDoesNotExistCases =
         {
-            var result = Solver.AnyWordExistsOnBoard(dictionaryC, board3x3);
-            Assert.That(result, Is.False);
-        }
+            new object[] { dictionaryB, board2x2 },
+            new object[] { dictionaryC, board2x2 },
+            new object[] { dictionaryC, board3x3 },
+        };
+
     }
 }
 
