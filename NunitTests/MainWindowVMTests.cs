0758c5e5106ae3b2580ae093fe2031bbf8eb0841
Tisa Foster <tisa.foster@gmail.com>
2017-04-25 17:20:08 -0600

    More MainWindowViewModel unit tests.

--- NunitTests/MainWindowVMTests.cs
+++ NunitTests/MainWindowVMTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Media;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using WordStrata;
+using Core;
 
 namespace NUnitTests
 {
@@ -13,12 +15,13 @@ namespace NUnitTests
     {
         HashSet<string> dictionary = new HashSet<string>() { "apple", "banana", "sailboat", "zebra" };
         MainWindowViewModel viewModel;
-
+        static Tile tileA = new Tile(new Coordinates(1, 2), 'a');
 
         [SetUp]
         public void Init()
         {
             viewModel = new MainWindowViewModel(new GameModel(dictionary));
+            viewModel.ThePath.Add(tileA);
         }
 
         [Test]
@@ -26,10 +29,22 @@ namespace NUnitTests
         {
             Assert.That(viewModel.GameBoard.Tiles, Is.Not.Empty);
         }
-    }
 
+        [Test]
+        public void CurrentSnake_ByDefault_ProducesCorrectGuiCoords()
+        {
+            var snake = viewModel.CurrentSnake;
+            Assert.That(snake.Points.Count.Equals(1));
+            Assert.That(snake.Points[0].X, Is.EqualTo(50).Within(0.01));
+            Assert.That(snake.Points[0].Y, Is.EqualTo(30).Within(0.01));
+        }
+
+        [Test]
+        public void EnableSubmit_ByDefault_IsCorrect()
+        {
+            Assert.That(viewModel.EnableSubmit.Equals(false));
+        }
+    }
 }
 // TODO: Tests on other public properties and methods
-    // CurrentSnake
-    // EnableSubmit
     // OnCollectionChanged error
