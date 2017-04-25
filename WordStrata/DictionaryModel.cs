2b386e225592148884263abed1690c549e762463
Tisa Foster <tisa.foster@gmail.com>
2017-04-25 11:25:09 -0600

    DictionaryModel exception handling.

--- WordStrata/DictionaryModel.cs
+++ WordStrata/DictionaryModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WordStrata
 {
@@ -11,15 +13,34 @@ namespace WordStrata
         public static HashSet<string> GetDictionary()
         {
             var list = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            string path = "sowpods.txt";
             string line;
-            System.IO.StreamReader file = new System.IO.StreamReader("sowpods.txt");
-            while ((line = file.ReadLine()) != null)
+            System.IO.StreamReader file = null;
+
+            try
             {
-                list.Add(line);
+                file = new System.IO.StreamReader(path);
+                while ((line = file.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
             }
-            file.Close();
+            catch (Exception ex) when (ex is FileNotFoundException || ex is IOException)
+            {
+                string messageBoxText = "Problem reading dictionary file: " + path + ". Message: " + ex.Message;
+                string caption = "File Error";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBox.Show(messageBoxText, caption, button, icon);
+            }
+            finally
+            {
+                if (file != null)
+                {
+                    file.Close();
+                }
+            }
             return list;
-            //TODO: file error handling?
         }
     }
 }
